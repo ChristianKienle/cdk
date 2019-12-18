0b865655bc8bb81fc814a404986199a302f5f53f
Christian Kienle <christian.kienle@sap.com>
2019-12-18 17:03:47 +0400

    fix: fixed link to storybook

---
+++ docs/docs/.vuepress/playground-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="$withBase('/storybook/')" class="playground-button">Playground</a>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style>
+.playground-button {
+  cursor: pointer;
+  position: fixed;
+  bottom: 2rem;
+  right: 2.5rem;
+  background: rgb(255, 71, 133);
+  border-radius: 3em;
+  color: white;
+  z-index: 1;
+  font-size: 14px;
+  font-weight: 800;
+  padding: 13px 20px;
+}
+</style>
