40db2733f8a806c1be55b2257e21f939f5862f81
Christian Kienle <christian.kienle@sap.com>
2019-12-23 23:09:33 +0400

    made playground button nicer + improved vp integration

--- docs/docs/.vuepress/playground-button.vue
+++ docs/docs/.vuepress/playground-button.vue
@@ -3,22 +3,31 @@
 </template>
 
 <script>
-
 export default {}
 </script>
 
-<style>
-.playground-button {
-  cursor: pointer;
-  position: fixed;
-  bottom: 2rem;
-  right: 2.5rem;
-  background: rgb(255, 71, 133);
-  border-radius: 3em;
-  color: white;
-  z-index: 1;
-  font-size: 14px;
-  font-weight: 800;
-  padding: 13px 20px;
-}
+<style lang="stylus">
+@import url('./styles/palette');
+.playground-button
+  cursor pointer
+  position fixed
+  bottom 2rem
+  right 2.5rem
+  background-color $accentColor
+  border-radius 3em
+  color white
+  z-index 1
+  font-size 12px
+  font-weight 500
+  padding 8px 11px
+  transform scale(1)
+  transition all .25s ease
+  &:hover
+    background-color: darken($accentColor, 30%)
+    transform scale(1.2)
+    box-shadow: 0 3px 12px 0 rgba(0,0,0,.3)
+  &:active
+    background-color: darken($accentColor, 30%)
+    transform scale(1.1)
+    box-shadow: 0 3px 6px 0 rgba(0,0,0,.4)
 </style>
