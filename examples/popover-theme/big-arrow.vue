27351db788ef54e52c12e9ef4a9a0e88c2cd3c66
Christian Kienle <christian.kienle@sap.com>
2019-12-23 18:04:55 +0400

    fixed popover examples

---
+++ examples/popover-theme/big-arrow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+    <CPopover theme="big-arrow" withArrow>
+      <template #trigger>
+        <button>Show 'big-arrow' Popover</button>
+      </template>
+      <div>I am a Popover. Wheeee. 😉</div>
+    </CPopover>
+  </div>
+</template>
+
+<script>
+import '@vue-cdk/popover/themes/big-arrow.css'
+export default {}
+</script>
---
+++ examples/popover-theme/clean.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+    <CPopover theme="clean" withArrow>
+      <template #trigger>
+        <button>Show 'big-arrow' Popover</button>
+      </template>
+      <div>I am a Popover. Wheeee. 😉</div>
+    </CPopover>
+  </div>
+</template>
+
+<script>
+import '@vue-cdk/popover/themes/clean.css'
+export default {}
+</script>
