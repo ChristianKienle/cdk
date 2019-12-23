7d504777bec64a4a54fa814db0b20b2b82c56912
Christian Kienle <christian.kienle@sap.com>
2019-12-23 18:04:55 +0400

    fixed popover examples

---
+++ examples/popover-theme/dark.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+    <CPopover theme="dark">
+      <template #trigger>
+        <button>Show 'dark' Popover</button>
+      </template>
+      <div>I am a Popover. Wheeee. 😉</div>
+    </CPopover>
+  </div>
+</template>
+
+<script>
+import '@vue-cdk/popover/themes/dark.css'
+export default {}
+</script>
---
+++ examples/popover-theme/v2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+    <CPopover theme="v2">
+      <template #trigger>
+        <button>Show 'dark' Popover</button>
+      </template>
+      <div>I am a Popover. Wheeee. 😉</div>
+    </CPopover>
+  </div>
+</template>
+
+<script>
+import '@vue-cdk/popover/themes/v2.css'
+export default {}
+</script>
