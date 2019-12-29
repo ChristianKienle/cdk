f6115773a0ad230fb9ad7677227d6c98fa330d9d
Christian Kienle <christian.kienle@sap.com>
2019-12-29 16:12:32 +0100

    feat: improved vuepress plugins

---
+++ packages/vuepress-plugin-demo/src/components/demo/copy-code-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="copy-code-button" v-on="$listeners">Copy</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+.copy-code-button
+  cursor pointer
+  background-color saturate(lighten($accentColor, 85%), 10%)
+  border-radius 8px
+  border 0
+  color saturate(darken($accentColor, 90%), 90%)
+  font-size 0.70rem
+  padding 4px 4px
+  margin 0
+  transform scale(1)
+  transition all .25s ease
+  outline none
+  &:hover
+    color white
+    background-color: darken($accentColor, 30%)
+  &:active
+    color white
+    background-color: darken($accentColor, 30%)
+</style>
