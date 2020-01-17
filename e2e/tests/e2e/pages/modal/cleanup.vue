982a80a7d1e362f50c85c905ec63bbda9f60004f
Christian Kienle <christian.kienle@sap.com>
2020-01-17 19:26:52 +0100

    made modals lazy

---
+++ e2e/tests/e2e/pages/modal/cleanup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li><button data-cy="activateTabA" @click="component = 'TabA'">tab A</button></li>
+      <li><button data-cy="activateTabB" @click="component = 'TabB'">tab B</button></li>
+    </ul>
+    <div>
+      <component :is="component" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TabA from './cleanup/tab-a.vue'
+import TabB from './cleanup/tab-b.vue'
+
+export default {
+  components: {
+    TabA,
+    TabB
+  },
+  data() {
+    return {
+      component: null
+    }
+  },
+  methods: {
+    tabAClicked() {},
+    tabBClicked() {}
+  }
+}
+</script>
---
+++ e2e/tests/e2e/pages/modal/cleanup/short-id.js
@@ -0,0 +1,8 @@
+export default () => {
+  let text = ''
+  const possible = 'abcdefghijklmnopqrstuvwxyz'
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
