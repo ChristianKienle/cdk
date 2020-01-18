f797647d000f2a8dad9ef711afdfe883053a42a5
Christian Kienle <christian.kienle@sap.com>
2020-01-18 14:20:46 +0100

    added bili

--- packages/modal/src/modal.vue
+++ packages/modal/src/modal.vue
@@ -1,19 +1,21 @@
 <template>
-  <Portal :selector="portalSelector">
-    <transition name="vcdk-modal-fade">
-      <slot name="overlay">
-        <Overlay v-if="open_" class="vcdk-modal__overlay">
-          <div :aria-hidden="String(!open_)" v-show="open_" style="outline: none">
-            <CFocusTrap :active="open_">
-              <div role="dialog" aria-modal="true" tabindex="-1" style="outline: none">
-                <slot :close="close" />
-              </div>
-            </CFocusTrap>
-          </div>
-        </Overlay>
-      </slot>
-    </transition>
-  </Portal>
+  <ClientOnly>
+    <Portal :selector="portalSelector">
+      <transition name="vcdk-modal-fade">
+        <slot name="overlay">
+          <Overlay v-if="open_" class="vcdk-modal__overlay">
+            <div :aria-hidden="String(!open_)" v-show="open_" style="outline: none">
+              <CFocusTrap :active="open_">
+                <div role="dialog" aria-modal="true" tabindex="-1" style="outline: none">
+                  <slot :close="close" />
+                </div>
+              </CFocusTrap>
+            </div>
+          </Overlay>
+        </slot>
+      </transition>
+    </Portal>
+  </ClientOnly>
 </template>
 
 <script>
@@ -22,11 +24,12 @@ import Overlay from './overlay.vue'
 import { Portal } from '@linusborg/vue-simple-portal'
 import shortId from './short-id'
 import onServer from '@vue-cdk/utils/on-server'
+import ClientOnly from '@vue-cdk/client-only/src/client-only.vue'
 
 export default {
   name: 'Modal',
   mixins: [CFocusTrap()],
-  components: { Overlay, Portal },
+  components: { ClientOnly, Overlay, Portal },
   props: {
     portalSelector: {
       default: () => `#${shortId()}`,
