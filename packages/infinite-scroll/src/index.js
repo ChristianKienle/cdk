5d513e0c047c901c072dd957e1d1d2ade7fc72a5
Christian Kienle <christian.kienle@sap.com>
2020-01-20 19:41:46 +0100

    adopted pluginify everywhere

--- packages/infinite-scroll/src/index.js
+++ packages/infinite-scroll/src/index.js
@@ -1,11 +1,7 @@
 import InfiniteScroll from './infinite-scroll.vue'
 import InfiniteScrollItem from './item.vue'
-import VueObserveVisibility from 'vue-observe-visibility'
 import pluginify from '@vue-cdk/pluginify'
 
-const plugin = pluginify(InfiniteScroll, InfiniteScrollItem)
+export default pluginify(InfiniteScroll, InfiniteScrollItem)
 
-export default (vue, options) => {
-  vue.use(plugin, options)
-  vue.use(VueObserveVisibility, options)
-}
+export { InfiniteScroll, InfiniteScrollItem }
--- packages/match-media/tests/e2e/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './app.vue'
-
-new Vue({
-  render(h) {
-    return h(App)
-  }
-}).$mount('#app')
