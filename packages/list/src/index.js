054651a561baf8fd279d5a1aa34538d559c20fe8
Christian Kienle <christian.kienle@sap.com>
2020-01-20 19:41:46 +0100

    adopted pluginify everywhere

--- packages/list/src/index.js
+++ packages/list/src/index.js
@@ -1,18 +1,10 @@
 import List from './list.vue'
 import ListItem from './item.vue'
-import VueObserveVisibility from 'vue-observe-visibility'
 import pluginify from '@vue-cdk/pluginify'
-
 import Single from './selection/single'
 import Multiple from './selection/multiple'
 
-const plugin = pluginify(List, ListItem)
+const SelectionMode = { Single, Multiple }
 
-function install(vue, options) {
-  vue.use(VueObserveVisibility, options)
-  vue.use(plugin, options)
-}
-
-export const SelectionMode = { Single, Multiple }
-
-export default install
+export default pluginify(List, ListItem)
+export { List, ListItem, SelectionMode }
--- packages/scroll-container/src/index.js
+++ packages/scroll-container/src/index.js
@@ -1,10 +1,5 @@
 import ScrollContainer from './scroll-container.vue'
 import pluginify from '@vue-cdk/pluginify'
 
-const plugin = pluginify(ScrollContainer)
-
-function install(vue, options) {
-  vue.use(plugin, options)
-}
-
-export default install
+export default pluginify(ScrollContainer)
+export { ScrollContainer }
