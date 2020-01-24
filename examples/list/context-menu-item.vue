8fdb396ab17578b1ee32ac926f8cc7607a084b53
Christian Kienle <christian.kienle@sap.com>
2020-01-24 23:04:22 +0100

    added styled list sample

---
+++ examples/list/context-menu-item.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="container">
+    <div class="vcdk-text identifier" :style="identifierStyle">
+      {{ item.index }}
+    </div>
+    <div class="vcdk-text main" style="flex: 2 0 0;"> Item with ID {{ item.id }} </div>
+    <div>
+      <CPopover
+        placement="bottom-end"
+        :body-styles="{ padding: 0 }"
+        theme="clean"
+        :visible="idState.menuOpen"
+        target="$refs.target"
+      >
+        <div class="vcdk-text menu">
+          <div class="menu-item">Delete Item</div>
+          <div class="menu-item">Duplicate Item</div>
+          <div class="menu-item">Share Item</div>
+          <div class="menu-item">Move Item</div>
+        </div>
+      </CPopover>
+      <button ref="target" class="context-button" @click="idState.menuOpen = !idState.menuOpen"
+        >more…</button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { IdState } from 'vue-virtual-scroller'
+const colors = [
+  '#BF5BAB',
+  '#D6648C',
+  '#E56973',
+  '#ED9982',
+  '#F9D68E',
+  '#5B79BF',
+  '#7564D6',
+  '#9D69E5',
+  '#CE82ED',
+  '#F98EE7'
+]
+export default {
+  mixins: [
+    IdState({
+      // You can customize this
+      idProp: vm => vm.item.id
+    })
+  ],
+  idState() {
+    return {
+      menuOpen: false
+    }
+  },
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    color() {
+      return colors[this.item.index % colors.length]
+    },
+    targetId() {
+      return `target-item-${this.item.id}`
+    },
+    identifierStyle() {
+      return {
+        width: '30px',
+        height: '30px',
+        'background-color': this.color
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+@import url('./utils.css');
+.menu {
+  display: flex;
+  flex-direction: column;
+  font-size: 0.75rem;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.menu-item {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.menu-item:hover {
+  background-color: rgb(0, 183, 255);
+  color: white;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  cursor: pointer;
+}
+.main {
+  padding-left: 10px;
+}
+.identifier {
+  border-radius: 25px;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 30px;
+  color: white;
+  font-size: 20px;
+}
+.container {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    'Noto Color Emoji';
+  display: flex;
+  align-items: center;
+}
+.context-button,
+.context-button:hover,
+context-button:focus,
+context-button:active {
+  cursor: pointer;
+  font-size: 0.75rem;
+  border: 1px solid #dddddd;
+  border-radius: 15px;
+  padding: 8px 12px;
+  outline: none;
+  background-color: transparent;
+  /* background-color: rgb(42, 195, 255); */
+  margin-right: 10px;
+  color: rgb(150, 150, 150);
+  color: rgb(0, 152, 212);
+}
+.context-button:hover {
+  background-color: #efefef;
+}
+.context-button:hover {
+  color: black;
+}
+</style>
---
+++ examples/list/context-menu-styled.vue
@@ -0,0 +1,77 @@
+<template>
+  <CList
+    :load-more="loadMore"
+    style="height: 100vh; outline: none;"
+    :selection-mode="selectionMode"
+    :items="items"
+    :min-item-size="40"
+  >
+    <template #default="{ item, active, index, selected }">
+      <CListItem
+        class="item"
+        :class="classesFor({ selected })"
+        :item="item"
+        :active="active"
+        :size-dependencies="[item.id]"
+      >
+        <CustomItem :item="item" />
+      </CListItem>
+    </template>
+  </CList>
+</template>
+
+<script>
+import '@vue-cdk/list/style/index.css'
+import '@vue-cdk/popover/themes/clean.css'
+import { SelectionMode } from '@vue-cdk/list'
+import CustomItem from './context-menu-item.vue'
+
+export default {
+  components: { CustomItem },
+  data: () => ({
+    selectionMode: SelectionMode.Single,
+    items: [...Array(100).keys()].map(id => ({ index: id, id }))
+  }),
+  methods: {
+    loadMore(done) {
+      const count = 100
+      const currentCount = this.items.length
+      const ids = Array.from({ length: count }).map((_, index) => index + currentCount)
+      const newItems = ids.map(id => ({ id, index: id }))
+      this.items.push(...newItems)
+      done()
+    },
+    targetIdFor(item) {
+      return `target-item-${item.id}`
+    },
+    classesFor({ selected }) {
+      return {
+        'item--selected': selected
+      }
+    }
+  }
+}
+</script>
+<style>
+*::-webkit-scrollbar {
+  width: 0 !important;
+}
+body {
+  margin: 0;
+}
+</style>
+<style scoped>
+.item {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    'Noto Color Emoji';
+  padding: 10px;
+}
+
+.item--selected {
+  z-index: -1;
+  background-color: #f9fafc;
+}
+</style>
