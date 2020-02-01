4f15a3e1911e9f031216d847d0c14181a552598e
Christian Kienle <christian.kienle@sap.com>
2020-02-01 17:14:53 +0100

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   chainWebpack: config => {
     // clear the existing entry point
-    config
-      .entry('app')
-        .clear()
+    config.entry('app').clear()
 
     // add your custom entry point
-    config
-      .entry('app')
-        .add('./e2e/main.js')
+    config.entry('app').add('./e2e/main.js')
   }
 }
