002212567db8fe32a5e20ab64cf5b6b53969ac0f
Christian Kienle <christian.kienle@sap.com>
2020-02-02 01:47:53 +0100

    feat: body lock

--- packages/modal/src/modal.vue
+++ packages/modal/src/modal.vue
@@ -1,7 +1,7 @@
 <template>
   <ClientOnly>
     <Portal :selector="portalSelector">
-      <transition name="vcdk-modal-fade">
+      <transition name="vcdk-modal-fade" @after-enter="afterEnter" @after-leave="afterLeave">
         <slot name="overlay">
           <Overlay v-if="open_" class="vcdk-modal__overlay">
             <div v-show="open_" :aria-hidden="String(!open_)" style="outline: none">
@@ -25,7 +25,7 @@ import { Portal } from '@linusborg/vue-simple-portal'
 import shortId from './short-id'
 import onServer from '@vue-cdk/utils/on-server'
 import ClientOnly from '@vue-cdk/client-only/src/client-only.vue'
-
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 export default {
   name: 'Modal',
   components: { ClientOnly, Overlay, Portal },
@@ -50,9 +50,15 @@ export default {
       this.open_ = open
     }
   },
+  created() {
+    this.$_lockedToTarget = null
+  },
   beforeDestroy() {
     if (onServer()) {
       return
+    }
+    if (this.$_lockedToTarget != null) {
+      enableBodyScroll(this.$_lockedToTarget)
     }
     const portalEl = document.querySelector(this.portalSelector)
     if (portalEl == null) {
@@ -61,6 +67,14 @@ export default {
     portalEl.parentNode.removeChild(portalEl)
   },
   methods: {
+    afterEnter(el) {
+      disableBodyScroll(el)
+      this.$_lockedToTarget = el
+    },
+    afterLeave(el) {
+      enableBodyScroll(el)
+      this.$_lockedToTarget = null
+    },
     close() {
       this.open_ = false
       this.$emit('update:open', false)
