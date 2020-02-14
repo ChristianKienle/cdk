b0b5641730534242fbac7e0b237af48708ae078e
Christian Kienle <christian.kienle@sap.com>
2020-02-14 17:07:31 +0100

    docs: added more examples

---
+++ packages/integration/__examples__/popover-with-input-as-target.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input
+      ref="input"
+      v-model="text"
+      @keydown.tab="hide"
+      @keyup.enter="hide"
+      @keyup.esc="hide"
+      @focus="show"
+      @click="show"
+    />
+    <input style="width: 200px;" value="Focus this by pressing tab" />
+    <CPopover
+      placement="bottom"
+      :target="() => $refs.input"
+      :visible="visible"
+      theme="clean"
+      with-arrow
+    >
+      <CPopoverContent>
+        <CInteractionOutside :ignored="() => [$refs.input]" @detected="visible = false">
+          <div>
+            <p>Text: {{ text }}</p>
+            <p>Enter/ESC/Tab: will hide the popover</p>
+            <p>onFocus/onClick: will show the popover</p>
+          </div>
+        </CInteractionOutside>
+      </CPopoverContent>
+    </CPopover>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: 'Edit me',
+      visible: false
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true
+    },
+    hide() {
+      this.visible = false
+    }
+  }
+}
+</script>
