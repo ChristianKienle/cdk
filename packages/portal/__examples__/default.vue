b27a6f16f3ebb151bde15610ec765c4c8c1d4a30
Christian Kienle <christian.kienle@sap.com>
2020-09-09 17:59:25 +0200

    fixed portal sample

---
+++ packages/portal/__examples__/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Before Portal Target</p>
+    <div id="target"></div>
+    <p>After Portal Target</p>
+    <p>Before Portal</p>
+
+      <CPortal selector="#target">
+        <div>Will be placed inside the #target-div.</div>
+      </CPortal>
+    <p>After Portal</p>
+  </div>
+</template>
+
+<script>
+import '@vue-cdk/popover/themes/clean.css'
+
+export default {
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.visible = !this.visible
+    },
+  },
+}
+</script>
