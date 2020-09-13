45b239434f635c66bec7df5f9bc49c260db97687
Christian Kienle <christian.kienle@sap.com>
2020-09-13 13:57:22 +0200

    fix

--- docs/docs/.vuepress/playground-package.js
+++ docs/docs/.vuepress/playground-package.js
@@ -1,53 +1,73 @@
+// @ts-check
+const Path = require('path')
+const globby = require('globby')
+const packagesRoot = Path.join(__dirname, '..', '..', '..', 'packages')
+const fs = require('fs')
+
+const packages = globby.sync(['**/package.json', '!**/node_modules'], {
+  cwd: packagesRoot,
+  gitignore: true,
+  absolute: true,
+})
+
+console.log(packages)
+
+const packageInfoFromPackageAtPath = (path) => {
+  const { version, name } = JSON.parse(fs.readFileSync(path, 'utf-8'))
+  return { version, name }
+}
+
+const dependencies = {
+  vue: '^2.6.11',
+  'vue-router': '^3.1.5',
+};
+
+const vueCdkPackageInfos = (() => {
+  return packages.map(packageInfoFromPackageAtPath)
+})()
+
+vueCdkPackageInfos.forEach(info => {
+  dependencies[info.name] = info.version
+})
+
 module.exports = {
-  "name": "vue-cdk-example",
-  "version": "0.1.0",
-  "private": true,
-  "scripts": {
-    "serve": "vue-cli-service serve",
-    "build": "vue-cli-service build",
-    "lint": "vue-cli-service lint"
+  name: 'vue-cdk-example',
+  version: '0.1.0',
+  private: true,
+  scripts: {
+    serve: 'vue-cli-service serve',
+    build: 'vue-cli-service build',
+    lint: 'vue-cli-service lint',
   },
-  "dependencies": {
-    "vue": "^2.6.11",
-    "vue-router": "^3.1.5",
-    "@vue-cdk/link": "^0.9.0",
-    "@vue-cdk/client-only": "^0.9.0",
-    "@vue-cdk/infinite-scroll": "^0.9.0",
-    "@vue-cdk/interaction-outside": "^0.9.0",
-    "@vue-cdk/link": "^0.9.0",
-    "@vue-cdk/list": "^0.9.0",
-    "@vue-cdk/match-media": "^0.9.0",
-    "@vue-cdk/modal": "^0.9.0",
-    "@vue-cdk/popover": "^0.9.0",
-    "@vue-cdk/focus-trap": "^0.9.0",
-    "@vue-cdk/portal": "^0.9.0",
-    "@vue-cdk/scroll-container": "^0.9.0",
-    "@vue-cdk/tooltip": "^0.9.0"
+  dependencies: {
+    vue: '^2.6.11',
+    'vue-router': '^3.1.5',
+    ...dependencies,
   },
-  "devDependencies": {
-    "@vue/cli-plugin-babel": "^3.6.0",
-    "@vue/cli-plugin-eslint": "^3.6.0",
-    "@vue/cli-service": "^3.6.0",
-    "babel-eslint": "^10.0.1",
-    "eslint": "^5.8.0",
-    "eslint-plugin-vue": "^5.0.0",
-    "vue-template-compiler": "^2.6.11"
+  devDependencies: {
+    '@vue/cli-plugin-babel': '^3.6.0',
+    '@vue/cli-plugin-eslint': '^3.6.0',
+    '@vue/cli-service': '^3.6.0',
+    'babel-eslint': '^10.0.1',
+    eslint: '^5.8.0',
+    'eslint-plugin-vue': '^5.0.0',
+    'vue-template-compiler': '^2.6.11',
   },
-  "eslintConfig": {
-    "root": true,
-    "env": {
-      "node": true
+  eslintConfig: {
+    root: true,
+    env: {
+      node: true,
     },
-    "extends": ["plugin:vue/essential", "eslint:recommended"],
-    "rules": {},
-    "parserOptions": {
-      "parser": "babel-eslint"
-    }
+    extends: ['plugin:vue/essential', 'eslint:recommended'],
+    rules: {},
+    parserOptions: {
+      parser: 'babel-eslint',
+    },
   },
-  "postcss": {
-    "plugins": {
-      "autoprefixer": {}
-    }
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
   },
-  "browserslist": ["> 1%", "last 2 versions", "not ie <= 8"]
+  browserslist: ['> 1%', 'last 2 versions', 'not ie <= 8'],
 }
