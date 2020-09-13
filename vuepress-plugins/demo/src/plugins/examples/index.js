f384101f067b9708b6256b870f41530ae36d515f
Christian Kienle <christian.kienle@sap.com>
2020-09-13 13:10:59 +0200

    bumps versions of multiple dependencies

--- vuepress-plugins/demo/src/plugins/examples/index.js
+++ vuepress-plugins/demo/src/plugins/examples/index.js
@@ -5,35 +5,35 @@ const componentNameFromComponents = require('./../component-name-from-components
 const Options = require('./../../options')
 
 /** @param {import('./../../options/types').Options} options */
-module.exports = options => {
+module.exports = (options) => {
   const _options = Options.normalize(options)
 
   const files = globby.sync(_options.examplesPattern, {
     ignore: ['**/__tests__/**'],
     absolute: true,
-    cwd: _options.cwd
+    cwd: _options.cwd,
   })
 
   const { collectionPathComponentsFromPath, packageNameFromPath, exampleNameFromPath } = _options
 
-  const exampleComponentNameComponentsFromPath = path => [
+  const exampleComponentNameComponentsFromPath = (path) => [
     'example',
     packageNameFromPath(path),
     ...collectionPathComponentsFromPath(path),
-    exampleNameFromPath(path)
+    exampleNameFromPath(path),
   ]
 
-  const exampleComponentNameFromPath = path =>
+  const exampleComponentNameFromPath = (path) =>
     componentNameFromComponents(exampleComponentNameComponentsFromPath(path))
-  const components = files.map(path => {
+  const components = files.map((path) => {
     return {
       path,
-      name: exampleComponentNameFromPath(path)
+      name: exampleComponentNameFromPath(path),
     }
   })
   return {
     // @ts-ignore
     name: require('./../../../package.json').name + '-examples',
-    plugins: [['@vuepress/register-components', { components }]]
+    plugins: [['@vuepress/register-components', { components }]],
   }
 }
